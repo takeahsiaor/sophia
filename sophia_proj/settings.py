831649f9097b0abf7c4db7b284b124bf5916af9a
Ron Hsiao <rhsiao2@gmail.com>
2017-02-01 18:49:17 -0600

    Remove overridden secret key. No point in having it

--- sophia_proj/settings.py
+++ sophia_proj/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lw&$6c_d*31mf1&uk!6wj+e+!olbhw&2p(_3$3khmnjol0bh9&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
